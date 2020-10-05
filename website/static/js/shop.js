e6d4e26bc70528e1c4a86502d112f37b9b376908
Tiago Lopes <63212742+tiagonlopes@users.noreply.github.com>
2020-10-05 03:49:58 -0500

    Add files via upload

--- website/static/js/shop.js
+++ website/static/js/shop.js
@@ -5,11 +5,16 @@ var dropDownList2 = d3.select("#selDataset2");
 var dropDownList3 = d3.select("#selDataset3");
 
 
-url = "http://127.0.0.1:5000/product_list"
+url = "http://127.0.0.1:5000/product/product_list"
 
 function init() {
     d3.json(url).then(function(data) {
-        var product = data.product_name;
+        var product = []
+        data.forEach(function(item) {
+            product.push(item.product_name);
+        });
+
+        console.log(product)
 
         var options = dropDownList1.selectAll("option")
             .data(product)
@@ -46,18 +51,17 @@ function updateProduct() {
     item1 = dropDownList1.property("value");
     item2 = dropDownList2.property("value");
     item3 = dropDownList3.property("value");
-    income = dropDownList4.property("value");
 
-    url2 = "http://127.0.0.1:5000/" + item1 + "/" + item2 + "/" + item3;
+
+    url2 = "http://127.0.0.1:5000/shop/" + item1 + "/" + item2 + "/" + item3;
 
     d3.json(url2, function(error, result) {
 
-            var infoPanel = d3.select("#nextproduct");
-            infoPanel.html("")
-            infoPanel.append("h5").text(`${result.product}, <\br> Find it in Department: ${result.department} in Aisle: ${result.aisle}`)
+        var infoPanel = d3.select("#nextproduct");
+        infoPanel.html("")
+        infoPanel.append("h5").text(`${result.product}, <\br> Find it in Department: ${result.department} in Aisle: ${result.aisle}`)
+    });
+};
 
-        };
-    };
 
-
-    init()
+init()
